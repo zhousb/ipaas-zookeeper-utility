88c2194d3c642e35f2787bf19dc7b102f5d08b23
zhousb <15067137047@163.com>
2016-05-30 18:03:07 +0800

    Signed-off-by: zhousb <15067137047@163.com>

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,14 @@
+import com.zsb.exception.ZkException;
+import com.zsb.zk.ZkPoolFactory;
+
 
 public class Test {
-
+	public static void main(String args[]){
+		try {
+			ZkPoolFactory.getZkPool("node01:2181/test", "test", "test");
+		} catch (ZkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
