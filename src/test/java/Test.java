f8ad1ea229464f7853e2582e6806eff09db98b7e
zhousb <15067137047@163.com>
2016-05-30 18:17:39 +0800

    Signed-off-by: zhousb <15067137047@163.com>

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,14 @@
 import com.zsb.exception.ZkException;
+import com.zsb.zk.ZkPool;
 import com.zsb.zk.ZkPoolFactory;
 
 
 public class Test {
 	public static void main(String args[]){
 		try {
-			ZkPoolFactory.getZkPool("node01:2181/test", "test", "test");
+			ZkPool zkPool = ZkPoolFactory.getZkPool("node01:2181/test", "test", "test");
+			
+			System.out.println("end!!!");
 		} catch (ZkException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
